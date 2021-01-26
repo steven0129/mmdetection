7427ba98fc51f72cd3090dd5871df3687bba49b7
steven0129 <h3584935@gmail.com>
2021-01-26 11:42:05 +0800

    Fix apis of testing process
    Signed-off-by: steven0129 <h3584935@gmail.com>

--- mmdet/apis/test.py
+++ mmdet/apis/test.py
@@ -26,7 +26,7 @@ def single_gpu_test(model,
         with torch.no_grad():
             result = model(return_loss=False, rescale=True, **data)
 
-        batch_size = 1
+        batch_size = len(result)
         if show or out_dir:
             if batch_size == 1 and isinstance(data['img'][0], torch.Tensor):
                 img_tensor = data['img'][0]
@@ -56,10 +56,11 @@ def single_gpu_test(model,
                     score_thr=show_score_thr)
 
         # encode mask results
-        # if isinstance(result[0], tuple):
-        #     result = [(bbox_results, encode_mask_results(mask_results))
-        #               for bbox_results, mask_results in result]
-        results.extend([result])
+        if isinstance(result[0], tuple):
+            result = [(bbox_results, encode_mask_results(mask_results))
+                      for bbox_results, mask_results in result]
+        results.extend(result)
+
         for _ in range(batch_size):
             prog_bar.update()
     return results
